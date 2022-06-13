825a8f0bafe11f9cdef0ca9d4dc06c3e25418522
Owusu-Desmond <desmond0406114009@gmail.com>
2022-06-13 18:32:19 +0000

    Fix errors

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const hamburger = document.querySelector(".Menu");
+const hamburger = document.querySelector('.Menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeBtn = document.querySelector('.close-btn')
+const closeBtn = document.querySelector('.close-btn');
 // When the menu is click
-hamburger.addEventListener('click', (event) => {
-    mobileMenu.classList.add('visible');
-    // disable scroll when mobile menu appear
-    document.body.style.overflow = 'hidden';
+hamburger.addEventListener('click', () => {
+  mobileMenu.classList.add('visible');
+  // disable scroll when mobile menu appear
+  document.body.style.overflow = 'hidden';
 });
 // close nav links when the close button is click
-closeBtn.addEventListener('click', (event) => {
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('visible');
+  // enable scroll when mobile menu appear
+  document.body.style.overflow = 'scroll';
+});
+mobileMenu.addEventListener('click', (event) => {
+  if (event.target.classList.contains('link')) {
+    // hide moblile menu
     mobileMenu.classList.remove('visible');
     // enable scroll when mobile menu appear
     document.body.style.overflow = 'scroll';
+  }
 });
-mobileMenu.addEventListener('click', (event) => {
-    if(event.target.classList.contains("link")){
-        // hide moblile menu    
-        mobileMenu.classList.remove('visible');
-        // enable scroll when mobile menu appear
-        document.body.style.overflow = 'scroll';
-    }
-})
